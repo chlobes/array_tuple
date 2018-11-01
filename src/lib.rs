f9791ef8edb27d9fabe2488bdb9e5e7fec891c02
Your Name <you@example.com>
2018-11-02 00:36:04 +1100

    implemented for Option

--- src/lib.rs
+++ src/lib.rs
@@ -5,34 +5,60 @@ pub trait ArrayTuple {
 	fn into_tuple(self) -> Self::Tuple;
 }
 
+impl<T: ArrayTuple> ArrayTuple for Option<T> {
+	type Array = Option<<T as ArrayTuple>::Array>;
+	type Tuple = Option<<T as ArrayTuple>::Tuple>;
+	fn into_array(self) -> Self::Array { self.map(|t| t.into_array()) }
+	fn into_tuple(self) -> Self::Tuple { self.map(|t| t.into_tuple()) }
+}
+
 /*impl<T> ArrayTuple for () {
 	type Array = [T; 0];
 	type Tuple = ();
 	fn into_array(self) -> [T; 0] { [] }
 	fn into_tuple(self) -> () { () }
-}
+}*/
 
 impl<T> ArrayTuple for [T; 0] {
 	type Array = Self;
 	type Tuple = ();
 	fn into_array(self) -> Self { [] }
 	fn into_tuple(self) -> () { () }
-}*/
+}
+
+/*macro_rules! impl_array_tuple {
+	$($($N: num),+) => {$(
+		impl<T> ArrayTuple for ($(T,$)+) { //$N times
+			type Array = [T; $N];
+			type Tuple = Self;
+			fn into_array(self) -> Self::Array { let ($(x$N,$)+)=self; [$(x$N,$)+] }
+			fn into_tuple(self) -> Self { self }
+		}
+		impl<T> ArrayTuple for [T; $N] {
+			type Array = Self;
+			type Tuple = ($(T,$)+); //$N times
+			fn into_array(self) -> Self { self }
+			fn into_tuple(self) -> Self::Tuple { let [$(x$N,$)+,]=self; ($(x$N,$)+) }
+		}
+	$)+}
+}
+
+impl_array_tuple(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32);*/
 
 use std::mem::transmute_copy;
 
 impl<T> ArrayTuple for (T,) {
 	type Array = [T; 1];
 	type Tuple = Self;
-	fn into_array(self) -> [T; 1] { unsafe{transmute_copy(&self)} }
-	fn into_tuple(self) -> (T,) { self }
+	fn into_array(self) -> Self::Array { let (x1,) = self; [x1] }
+	fn into_tuple(self) -> Self { self }
 }
 
 impl<T> ArrayTuple for [T; 1] {
 	type Array = Self;
 	type Tuple = (T,);
 	fn into_array(self) -> Self { self }
-	fn into_tuple(self) -> (T,) { unsafe{transmute_copy(&self)} }
+	fn into_tuple(self) -> Self::Tuple { let [x1] = self; (x1,) }
 }
 
 impl<T> ArrayTuple for (T, T) {
